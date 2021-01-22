a46039669db82540aa3e78e93baab09a0943e3d4
DebbieG <splurge22@yahoo.com>
2021-01-21 21:34:00 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FilterComponent } from './filter/filter.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FilterComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
